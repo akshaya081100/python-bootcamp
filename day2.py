6763c19b77f57e32703fd163ad446dbe6a81b37b
akshaya081100 <85818941+akshaya081100@users.noreply.github.com>
2021-06-13 20:14:42 +0530

    Update day2.py

--- day2.py
+++ day2.py
@@ -1,1 +1,61 @@
+print("30 days 30 hour challenge")
+print('30 days 30 hour challenge')
+
+Hours = "thirty"
+print(Hours)
+
+myname="Akshaya"
+print(myname[2])
+
+Challenge="I will win"
+print(Challenge[2:5])
+
+myname="Akshaya"
+print(len(myname))
+
+myname="Akshaya"
+print(Challenge.lower())
+
+
+a="Hello"
+b="World"
+c= a + b
+print (c)
+
+
+a= "hello"
+b= "world"
+c= a+ " " + b
+print(c)
+
+
+text = "Thirty days and Thirty hours"
+
+x = text.casefold()
+print(x)
+
+
+x = text.capitalize()
+print(x)
+
+
+x = text.find('e')
+print(x)
+
+
+x = text.isalpha()
+print(x)
+
+
+x = text.isalnum()
+print(x)
+
+
+
+
+
+
+
+
+
 
