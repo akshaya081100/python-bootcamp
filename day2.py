c315c5c832ebb7107fbe6bc5b94e3948c298a6d0
akshaya081100 <85818941+akshaya081100@users.noreply.github.com>
2021-06-13 20:34:28 +0530

    Update day2.py

--- day2.py
+++ day2.py
@@ -1,34 +1,40 @@
+# printing a value
 print("30 days 30 hour challenge")
 print('30 days 30 hour challenge')
 
+# assigning a value to a string
 Hours = "thirty"
 print(Hours)
 
+#indexing using string
 myname="Akshaya"
 print(myname[2])
 
+#printing the required character from the string
 Challenge="I will win"
 print(Challenge[2:5])
 
+# printing the length of the character
 myname="Akshaya"
 print(len(myname))
 
+#converting the string into lower character
 myname="Akshaya"
-print(Challenge.lower())
+print(myname.lower())
 
-
+#concatenating two strings
 a="Hello"
 b="World"
 c= a + b
 print (c)
 
-
+# adding space between two strings
 a= "hello"
 b= "world"
 c= a+ " " + b
 print(c)
 
-
+# casefold()-usage
 text = "Thirty days and Thirty hours"
 
 x = text.casefold()
