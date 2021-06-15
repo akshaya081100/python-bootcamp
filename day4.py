447103da240b43bb4d6fe4d40f1f09c1331140a1
akshaya081100 <85818941+akshaya081100@users.noreply.github.com>
2021-06-15 22:51:27 +0530

    Update day4.py

--- day4.py
+++ day4.py
@@ -1,1 +1,28 @@
+#)Create three variables (a,b,c) to same value of any integer & do the following a) Divide a by 10 b) Multiply b by 50 c) Add c value by 60
 
+a = b = c = 45
+print("a="  "b="  "c=" , a,b,c)
+d=(a/10)
+e=(b*50)
+f=(c+60)
+print(d)
+print(e)
+print(f)
+
+#Create a String variable of 5 characters and replace the 3rd character with G
+
+a="world"
+print("The original word is",a)
+b = a.replace("r" , "G")
+print("the new word is",b)
+
+#Create two values (a,b) of int,float data type & convert the vise versa
+
+a = 5
+b = 6.5
+print(a)
+print(b)
+c = float(a)
+d = int(b)
+print("new a value",c)
+print("new b value",d)
